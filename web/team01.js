8dad0a27592aa9b1502ed8c3974e23e903090732
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-04-29 17:53:29 -0600

    Removing bg-dark and adding optional color jquery

--- web/team01.js
+++ web/team01.js
@@ -13,6 +13,11 @@ $(document).ready(function(){
     console.log("step 1, ready");
     $("#jQcolorChanger").click(function(){
         console.log("step 2, clicked");
+        var div1Class = $("#div1").attr("class");
+        $("#div1").removingClass("bg-dark").addClass( "bg-" + $("input".val()) );
+
+
+
         $("#div1").css("background-color", $("input").val());
         console.log("step 3, changed color");
     });
