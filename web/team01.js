e6151b8fbe8f2bf15037e499e7348ffc018544f8
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-04-29 14:56:30 -0600

    button and color changer

--- web/team01.js
+++ web/team01.js
@@ -1,3 +1,9 @@
 function alertClick(){
     alert("Clicked!");
 }
+
+function changeColor(){
+    var color = document.getElementById("colorChoice").value;
+    var div1 = document.getElementById("div1");
+    div1.style.backgroundColor = color;
+}
