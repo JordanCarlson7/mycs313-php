59def02afb8b69ad88926a4f5470e718b2049ff7
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-04-29 18:06:49 -0600

    comments and testing universal bg- class remover

--- web/team01.js
+++ web/team01.js
@@ -2,6 +2,7 @@ function alertClick(){
     alert("Clicked!");
 }
 
+/*JS changing of background color Not-in-use*/
 function changeColor(){
     var color = document.getElementById("colorChoice").value;
     var div1 = document.getElementById("div1");
@@ -13,15 +14,22 @@ $(document).ready(function(){
     console.log("step 1, ready");
     $("#jQcolorChanger").click(function(){
         console.log("step 2, clicked");
+
+        /*Bootstrap color change CHANGES CLASS*/
         var div1Class = $("#div1").attr("class");
-        $("#div1").removingClass("bg-dark").addClass( "bg-" + $("input".val()) );
+        console.log(div1Class);
+        var newBackground = $("input").val();
+        $("#div1").removeClass("bg\S+")
+        $("#div1").addClass( "bg-" + newBackground);
 
 
-
+        /*Non Bootstrap background color change
         $("#div1").css("background-color", $("input").val());
+        */
         console.log("step 3, changed color");
     });
 
+    /*Fade in and Fade out Div 3 with same button*/
     $("#jQfadeToggle").click(function(){
         console.log("fade toggle clicked");
         $("#div3").fadeToggle(1000);
