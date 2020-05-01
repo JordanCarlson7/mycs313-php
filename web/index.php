1d5d4eecf22d7e666c80086dd4566a4eb0ab85a7
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-04-30 20:39:21 -0600

    index and dynamic allocator for directory

--- web/index.php
+++ web/index.php
@@ -1,5 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="homePage.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="homePage.js"></script>
+    </head>
+    <body onload="request()">
+      <div id="table"></div>
+</body>
+</html>
+
+
 <?php
 
-  phpinfo();
+  echo "idk";
 
 ?>
