e003edb6d415411dc7be0ef4dde05fb8fe1c64d2
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-06 17:32:55 -0600

    pictures

--- web/browseItems.php
+++ web/browseItems.php
@@ -11,25 +11,25 @@ $boat = new Item();
 $boat->name = "boat";
 $boat->price = "8000";
 $boat->description = "Large personal yacht for friends and family";
-$boat->imgfile = "";
+$boat->imgfile = "yacht.jpg";
 
 $plane = new Item();
 $plane->name = "plane";
 $plane->price = "15000";
 $plane->description = "Small bush plane for leisure and travel";
-$plane->imgfile = "";
+$plane->imgfile = "biPlane.jpg";
 
 $guitar = new Item();
 $guitar->name = "guitar";
 $guitar->price = "3000";
 $guitar->description = "Jam out and become a rock star or family band-member";
-$guitar->imgfile = "";
+$guitar->imgfile = "guitar.jpg";
 
 $soda = new Item();
 $soda->name = "soda";
 $soda->price = "1";
 $soda->description = "sit back and enjoy a refreshing beverage";
-$soda->imgfile = "";
+$soda->imgfile = "sodaBottle.jpg";
 
 $items = [
     "Boat" => $boat,
@@ -66,13 +66,13 @@ $items = [
             <?= $item . ", $" . $obj->price ?>
             <p> <?= $obj->description ?></p>
             <img src="<?= $obj->imgfile ?>" alt="<?= $item ?>">
-            <button type="button" id="<? $item . $obj->price ?>" onclick="addToCart()" class="bg-dark">
+            <button type="button" id="<? $item . $obj->price ?>" onclick="addToCart()" class="bg-dark">Add To Cart</button>
         </div>
     </div>
     <?php endforeach;?>
     </div>
     <div class="col-sm-4">
-    <button type="button" id="toCart" onclick="toCart()" class="bg-dark">
+    <button type="button" id="toCart" onclick="toCart()" class="bg-dark">View Cart</button>
     </div>
 </div>
 <footer class="bg-dark navbar-dark display-3">Footer</footer>
