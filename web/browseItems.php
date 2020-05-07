61451b5c98c4b446429f7827b311cd30f89f39a6
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-06 19:12:32 -0600

    json encoding php object

--- web/browseItems.php
+++ web/browseItems.php
@@ -67,13 +67,13 @@ $items = [
             <?= $item . ", $" . $obj->price ?>
             <p> <?= $obj->description ?></p>
             <img src="<?= $obj->imgfile ?>" alt="<?= $item ?>">
-            <button type="button" id="<?= $item . "button" ?>" onclick="addToCart(this)" class="bg-dark">Add To Cart</button>
+            <button type="button" id="<?= $item . "button" ?>" data-object="<?=json_encode($obj)?>" onclick="addToCart(this)" class="bg-dark light">Add To Cart</button>
         </div>
     </div>
     <?php endforeach;?>
     </div>
     <div class="col-sm-4">
-    <a href="viewCart.php" id="toCart" class="bg-dark">View Cart</a>
+    <a href="viewCart.php" id="toCart" class="bg-dark light">View Cart</a>
     </div>
 </div>
 <?php include "footer03.php" ?>
--- web/shoppingCart.js
+++ web/shoppingCart.js
@@ -1,3 +1,20 @@
 function addToCart(itemName){
     console.log(itemName);
 }
+
+
+function postItem(item){
+   
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        console.log(this.readyState);
+        console.log(this.status);
+        if (this.readyState == 4 && this.status == 200) {
+         
+          
+          
+        }
+      };
+      xhttp.open("POST", "addToCart.php" + "?=" , true);//open(method, url, async);
+      xhttp.send();
+}
