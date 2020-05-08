ad3c71db61f78e74928a3f9518521119648b243e
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-08 00:13:01 -0600

    take 4 php session upload

--- web/addSessionVariables.php
+++ web/addSessionVariables.php
@@ -5,5 +5,8 @@ echo $_POST["name"];
 $changingValue = $_SESSION[$_POST["name"]];
 echo $changingValue->name;
 $changingValue->added = 1;
+foreach ($changingValue as $prop => $value){
+    echo $prop . "= " . $value;
+}
 
 ?>
