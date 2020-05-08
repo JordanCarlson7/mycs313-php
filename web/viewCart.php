abdaccc66230b28604e3c3596fd674e8aa50bab2
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-08 12:25:32 -0600

    getting from session

--- web/viewCart.php
+++ web/viewCart.php
@@ -1,1 +1,51 @@
-<?php echo "yo" ?>
+<?php 
+class Item {
+    public $name;
+    public $price;
+    public $description;
+    public $imgfile;
+    public $added;
+}
+session_start();
+$items = array();
+$temp = new Item();
+foreach($_SESSION as $key => $value){
+    $temp = $_SESSION[$key];
+    
+    if($temp->added < 0){
+        $items[$key] = $temp;
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">    
+<head>
+<title>Item Browser</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="shoppingCart.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="shoppingCart.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<?php include "headNav03.php"?>
+
+<div class="row">
+    <div class="col-sm-8">
+    <?php foreach ($items as $item => $obj):?>
+        <div class="row" id="<?= $item ?>" data-name="<?=$obj->name?>" data-description="<?= $obj->description ?>" data-price="<?= $obj->price ?>" data-imgfile="<?= $obj->imgfile ?>" data-added="<?= $obj->added ?>">
+        <div>
+            <?= $item . ", $" . $obj->price ?>
+            <p> <?= $obj->description ?></p>
+            <img src="<?= $obj->imgfile ?>" alt="<?= $item ?>">
+            <button type="button" id="<?= $item . "button" ?>" data-object="<?= json_encode($obj)?>" onclick="removeFromCart(this)" class="bg-dark light">Remove From Cart</button>
+        </div>
+    </div>
+    <?php endforeach;?>
+    </div>
+</div>
+<?php include "footer03.php" ?>
+</body>
+</html>
