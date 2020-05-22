6648a5d541363e172a25b1f99a053d3714edcd1d
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-21 22:27:28 -0600

    full request

--- web/access_Progress_DB.php
+++ web/access_Progress_DB.php
@@ -28,7 +28,7 @@ if (isset($_GET['search'])) {
   
 
 if (!isset($searchTerm)) {
-  $stmt = $db->prepare('SELECT * FROM profiles');
+  $stmt = $db->prepare('SELECT * FROM profiles, schedules, projects, data_points');
 } else {
   $stmt = $db->prepare('SELECT * FROM profiles, schedules, projects, data_points WHERE profiles.user_name = :user_name');
   $stmt->bindValue(':user_name', $searchTerm, PDO::PARAM_INT);
