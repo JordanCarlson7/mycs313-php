bb59485f4e7813849758f552a521d8558f0ef643
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-29 16:50:22 -0600

    here we go first update

--- web/updateProgress.php
+++ web/updateProgress.php
@@ -1,15 +1,28 @@
 <?php
+
 require 'accessDB.php';
 
     foreach($_POST as $key => $value){
         echo $key . ": " . $value;
     }
 
-    /*
-  $stmt = $db->prepare('INSERT INTO data_point FROM data_points WHERE data_points.user_name = :user_name');
-  $stmt->bindValue(':user_name', $username, PDO::PARAM_INT);
+  $db = getDB();
+  $stmt = $db->prepare('INSERT INTO schedules (user_name, schedule_id) VALUES (:username, :schedule)');
+  $stmt->bindValue(':username', $_POST['username'], PDO::PARAM_INT);
+  $stmt->bindValue(':schedule', $_POST['schedule'], PDO::PARAM_INT);
   
   $stmt->execute();
-  */
+
+  
+  $stmt = $db->prepare('INSERT INTO projects (user_name, schedule_id, project_id, description, timeline) VALUES (:username, :schedule, :project, :description_project, (:timelineStart, :timelineEnd)');
+  $stmt->bindValue(':username', $_POST['username'], PDO::PARAM_INT);
+  $stmt->bindValue(':schedule', $_POST['schedule'], PDO::PARAM_INT);
+  $stmt->bindValue(':project', $_POST['project'], PDO::PARAM_INT);
+  $stmt->bindValue(':description_project', $_POST['description_project'], PDO::PARAM_INT);
+  $stmt->bindValue(':timelineStart', $_POST['startDate'], PDO::PARAM_INT);
+  $stmt->bindValue(':timelineEnd', $_POST['endDate'], PDO::PARAM_INT);
+  
+  
+  $stmt->execute();
 
 ?>
