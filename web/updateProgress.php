d03baa5d39cf15691d4a1de31179a2c8e5207b3b
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-29 22:07:37 -0600

    no timeline

--- web/updateProgress.php
+++ web/updateProgress.php
@@ -19,13 +19,13 @@ $schedule = $_POST['schedule'];
   $stmt->execute();
 
   
-  $stmt = $db->prepare('INSERT INTO projects (user_name, schedule_id, project_id, description, timeline) VALUES (:username, :schedule, :project, :description_project, (:timelineStart, :timelineEnd)');
+  $stmt = $db->prepare('INSERT INTO projects (user_name, schedule_id, project_id, description, timeline) VALUES (:username, :schedule, :project, :description_project)');
   $stmt->bindValue(':username', $_POST['username'], PDO::PARAM_INT);
   $stmt->bindValue(':schedule', $_POST['schedule'], PDO::PARAM_INT);
   $stmt->bindValue(':project', $_POST['project'], PDO::PARAM_INT);
   $stmt->bindValue(':description_project', $_POST['description_project'], PDO::PARAM_INT);
-  $stmt->bindValue(':timelineStart', $_POST['startDate'], PDO::PARAM_INT);
-  $stmt->bindValue(':timelineEnd', $_POST['endDate'], PDO::PARAM_INT);
+  //$stmt->bindValue(':timelineStart', $_POST['startDate'], PDO::PARAM_INT);
+  //$stmt->bindValue(':timelineEnd', $_POST['endDate'], PDO::PARAM_INT);
   
   
   $stmt->execute();
