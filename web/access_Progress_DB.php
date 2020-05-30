6fdcf329a9bf86d20390539d6592fc62ce09b2b9
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-30 16:43:56 -0600

    AGAIN

--- web/access_Progress_DB.php
+++ web/access_Progress_DB.php
@@ -5,27 +5,20 @@ $db = getDB();
 foreach ($_POST as $key => $value){
   echo $key . ": " . $value;
 }
-/*
+
 if (isset($_POST['username'])) {
     $username = $_POST['username'];
     $schedule = $_POST['schedule'];
     $project = $_POST['project'];
 }
   
-if (!isset($username)) {
-  $stmt = $db->prepare('');
-} else {
-  $stmt = $db->prepare('SELECT title, description, data_d, attach1, attach2, attach3 FROM data_points WHERE data_points.user_name = :user_name AND data_points.project_id = :project_id');
-  $stmt->bindValue(':user_name', $username, PDO::PARAM_INT);
-  $stmnt->bindValue(':project_id', $project, PDO::PARAM_INT);
-}
-
+$stmt = $db->prepare('SELECT title, description, data_d, attach1, attach2, attach3 FROM data_points WHERE data_points.user_name = :user_name AND data_points.project_id = :project_id');
+$stmt->bindValue(':user_name', $username, PDO::PARAM_INT);
+$stmnt->bindValue(':project_id', $project, PDO::PARAM_INT);
 
 $stmt->execute();
 $datas = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $returnString = json_encode($datas);
 echo $returnString;
-*/
-
 ?>
