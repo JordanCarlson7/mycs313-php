9a5a80c9c6a77179ce50c0d81de610991f08479e
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-30 17:01:34 -0600

    ?

--- web/updateProgress.php
+++ web/updateProgress.php
@@ -13,25 +13,23 @@ $schedule = $_POST['schedule'];
   
   $stmt = $db->prepare('INSERT INTO schedules (user_name, schedule_id) VALUES (:username, :schedule)');
 
-  $stmt->bindValue(':username', $username, PDO::PARAM_INT);
-  $stmt->bindValue(':schedule', $schedule, PDO::PARAM_INT);
+  $stmt->bindValue(':username', $username);
+  $stmt->bindValue(':schedule', $schedule);
   
   $stmt->execute();
 
-  $username = $_POST['username'];
-  $schedule = $_POST['schedule'];
   $project = $_POST['project'];
   $description_project = $_POST['description_project'];
   $startDate = $_POST['startDate'];
   
-  $stmt = $db->prepare('INSERT INTO projects (user_name, schedule_id, project_id, description, start_d, end_d) VALUES (:username, :schedule, :project, :description_project, :timelineStart, :timelineEnd)');
-  $stmt->bindValue(':username', $username);
-  $stmt->bindValue(':schedule', $schedule);
-  $stmt->bindValue(':project', $project);
-  $stmt->bindValue(':description_project', $description_project);
-  $stmt->bindValue(':timelineStart', $startDate);
-  $stmt->bindValue(':timelineEnd', $endDate);
+  $stmt1 = $db->prepare('INSERT INTO projects (user_name, schedule_id, project_id, description, start_d, end_d) VALUES (:username, :schedule, :project, :description_project, :timelineStart, :timelineEnd)');
+  $stmt1->bindValue(':username', $username);
+  $stmt1->bindValue(':schedule', $schedule);
+  $stmt1->bindValue(':project', $project);
+  $stmt1->bindValue(':description_project', $description_project);
+  $stmt1->bindValue(':timelineStart', $startDate);
+  $stmt1->bindValue(':timelineEnd', $endDate);
   
   
-  $stmt->execute();
+  $stmt1->execute();
 ?>
