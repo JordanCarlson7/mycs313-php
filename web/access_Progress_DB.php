1ee925b22bc9fa599c709fe584265c26b2cab263
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-30 18:07:18 -0600

    no isset

--- web/access_Progress_DB.php
+++ web/access_Progress_DB.php
@@ -5,12 +5,6 @@ $db = getDB();
 foreach ($_POST as $key => $value){
   //echo $key . ": " . $value;
 }
-
-if (isset($_POST['username'])) {
-    $username = $_POST['username'];
-    $schedule = $_POST['schedule'];
-    $project = $_POST['project'];
-}
   
 $stmt = $db->prepare('SELECT title, description, data_d, attach1, attach2, attach3 FROM data_points WHERE data_points.user_name = :user_name AND data_points.project_id = :project_id');
 $stmt->bindValue(':user_name', $username, PDO::PARAM_STR);
