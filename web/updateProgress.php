cf562e250ea7f11b3eceab491b70cac1fffe9293
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-30 20:41:12 -0600

    insert data point try 1

--- web/updateProgress.php
+++ web/updateProgress.php
@@ -32,5 +32,27 @@ $schedule = $_POST['schedule'];
   $stmt1->bindValue(':timelineEnd', $endDate, PDO::PARAM_STR);
   
   
-  $stmt1->execute();
+  $stmt2->execute();
+
+  $title = $_POST['title'];
+  $description_dataPoint = $_POST['description_data'];
+  $data_d = $_POST['data_d'];
+  $attach1 = $_POST['attach1'];
+  $attach2 = $_POST['attach2'];
+  $attach3 = $_POST['attach3'];
+
+  $stmt2 = $db->prepare('INSERT INTO data_points (user_name, project_id, title, description, data_d, attach1, attach2, attach3) VALUES (:username, :project_id, :title, :description_data, :data_d, :attach1, :attach2, :attach3)');
+  $stmt2->bindValue(':username', $username, PDO::PARAM_STR);
+  $stmt2->bindValue(':project_id', $project, PDO::PARAM_STR);
+  $stmt2->bindValue(':title', $title, PDO::PARAM_STR);
+  $stmt2->bindValue(':description_data', $description_dataPoint, PDO::PARAM_STR);
+  $stmt2->bindValue(':data_d', $data_d, PDO::PARAM_STR);
+  $stmt2->bindValue(':attach1', $attach1, PDO::PARAM_STR);
+  $stmt2->bindValue(':attach2', $attach2, PDO::PARAM_STR);
+  $stmt2->bindValue(':attach3', $attach3, PDO::PARAM_STR);
+  
+  
+  $stmt2->execute();  
+
+
 ?>
