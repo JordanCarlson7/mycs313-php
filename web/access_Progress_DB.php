23196bb66e7f1dac7f212f4c295d716a123d07f5
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-30 20:13:19 -0600

    stmnt

--- web/access_Progress_DB.php
+++ web/access_Progress_DB.php
@@ -7,14 +7,11 @@ foreach ($_POST as $key => $value){
 
 $username = $_POST['username'];
 $schedule = $_POST['schedule'];
-var_dump($_POST);
-die();
-$stmt = $db->prepare('SELECT * FROM data_points INNER JOIN projects ON data_points.user_name = projects.user_name WHERE projects.user_name = :username');
-$stmt->bindValue(':username', $username, PDO::PARAM_STR);
-$stmnt->bindValue(':project_id', $project, PDO::PARAM_STR);
 
-var_dump($stmt);
-die();
+$stmt = $db->prepare('SELECT * FROM data_points INNER JOIN projects ON data_points.user_name = projects.user_name WHERE projects.user_name = :username');
+
+$stmt->bindValue(':username', $username, PDO::PARAM_STR);
+$stmt->bindValue(':project_id', $project, PDO::PARAM_STR);
 $stmt->execute();
 $datas = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
