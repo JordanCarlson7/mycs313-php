4d239e40a32b34417d4b2a5a9a04704efb29a07e
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-30 18:13:13 -0600

    lol added variables

--- web/access_Progress_DB.php
+++ web/access_Progress_DB.php
@@ -5,9 +5,12 @@ $db = getDB();
 foreach ($_POST as $key => $value){
   //echo $key . ": " . $value;
 }
+
+$username = $_POST['username'];
+$schedule = $_POST['schedule'];
   
-$stmt = $db->prepare('SELECT title, description, data_d, attach1, attach2, attach3 FROM data_points WHERE user_name = :user_name AND project_id = :project_id');
-$stmt->bindValue(':user_name', $username, PDO::PARAM_STR);
+$stmt = $db->prepare('SELECT title, description, data_d, attach1, attach2, attach3 FROM data_points WHERE data_points.user_name = :username AND data_points.project_id = :project_id');
+$stmt->bindValue(':username', $username, PDO::PARAM_STR);
 $stmnt->bindValue(':project_id', $project, PDO::PARAM_STR);
 
 $stmt->execute();
