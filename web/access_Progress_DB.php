c11cf072a605c24eac5626a8365e47a8b2a705d2
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-05-30 18:18:21 -0600

    select user and proj

--- web/access_Progress_DB.php
+++ web/access_Progress_DB.php
@@ -9,7 +9,7 @@ foreach ($_POST as $key => $value){
 $username = $_POST['username'];
 $schedule = $_POST['schedule'];
   
-$stmt = $db->prepare('SELECT title, description, data_d, attach1, attach2, attach3 FROM data_points WHERE data_points.user_name = :username AND data_points.project_id = :project_id');
+$stmt = $db->prepare('SELECT user_name, project_id, title, description, data_d, attach1, attach2, attach3 FROM data_points WHERE data_points.user_name = :username AND data_points.project_id = :project_id');
 $stmt->bindValue(':username', $username, PDO::PARAM_STR);
 $stmnt->bindValue(':project_id', $project, PDO::PARAM_STR);
 
