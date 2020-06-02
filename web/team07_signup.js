2cf3df2b4ae368038182618050c767cfe6901238
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-06-02 16:43:21 -0600

    client valid

--- web/team07_signup.js
+++ web/team07_signup.js
@@ -1,4 +1,25 @@
+
+function check_valid_pwd(password) {
+    if (password.match('/^(?=\D*\d)[\w\d]{7,}$/') != password) {
+        return false;
+    }
+    return true;
+}
+
+function check_valid_username(username) {
+    return true;
+}
+
 function verify_signup(username_ID, password_ID, verify_ID, message_ID) {
+    let username = document.getElementById(username_ID).value;
+    let password = document.getElementById(password_ID).value;
+    let verify = document.getElementById(verify_ID).value;
+
+    if (!check_valid_pwd(password) || !check_valid_username(username)
+    || password != verify) {
+        document.getElementById(message_ID).innerText = "Invalid Usrname or Password";
+    }
+
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
@@ -22,9 +43,7 @@ function verify_signup(username_ID, password_ID, verify_ID, message_ID) {
         }
     }
 
-    let username = document.getElementById(username_ID).value;
-    let password = document.getElementById(password_ID).value;
-    let verify = document.getElementById(verify_ID).value;
+
     xhttp.open("POST", "team07_try_signup.php", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send("username=" + username + "&password=" + password + "&verify_password=" + verify);
