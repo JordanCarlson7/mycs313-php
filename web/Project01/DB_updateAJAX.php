e8eae060675e241f7a75e41131bd48608765e6d2
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-06-06 16:15:21 -0600

    restructure

--- web/Project01/DB_updateAJAX.php
+++ web/Project01/DB_updateAJAX.php
@@ -17,7 +17,7 @@ $db = getDB();
   if (isset($_POST['project'])){
     newProject($db);
   }
-  if(isset($_POST['title']) || isset($_POST['titlep'])){
+  if(isset($_POST['title'])){
     if (isset($_POST['project'])){
       $project = $_POST['project'];
       $username = $_POST['username'];
@@ -27,18 +27,22 @@ $db = getDB();
       $attach1 = $_POST['attach1'];
       $attach2 = $_POST['attach2'];
       $attach3 = $_POST['attach3'];
+      newData($db, $project, $username, $title, $description_dataPoint, $data_d, $attach1, $attach2, $attach3);
     }
-    else if (isset($_POST['projectp'])){
-      $project = $_POST['projectp'];
-      $username = $_POST['usernamep'];
-      $title = $_POST['titlep'];
-      $description_dataPoint = $_POST['description_datap'];
-      $data_d = $_POST['data_dp'];
-      $attach1 = $_POST['attach1p'];
-      $attach2 = $_POST['attach2p'];
-      $attach3 = $_POST['attach3p'];
+    if(isset($_POST['titlep'])){
+      if (isset($_POST['projectp'])){
+        $project = $_POST['projectp'];
+        $username = $_POST['usernamep'];
+        $title = $_POST['titlep'];
+        $description_dataPoint = $_POST['description_datap'];
+        $data_d = $_POST['data_dp'];
+        $attach1 = $_POST['attach1p'];
+        $attach2 = $_POST['attach2p'];
+        $attach3 = $_POST['attach3p'];
+      }
+      newData($db, $project, $username, $title, $description_dataPoint, $data_d, $attach1, $attach2, $attach3);
     }
-    newData($db, $project, $username, $title, $description_dataPoint, $data_d, $attach1, $attach2, $attach3);
+    
   }
 
   function newSchedule($db){
@@ -68,7 +72,6 @@ $db = getDB();
   $stmt1->bindValue(':timelineEnd', $endDate, PDO::PARAM_STR);
   $stmt1->execute();
   }
-
 
   function newData($db, $project, $username, $title, $description_dataPoint, $data_d, $attach1, $attach2, $attach3){
   $stmt2 = $db->prepare('INSERT INTO data_points (user_name, project_id, title, description, data_d, attach1, attach2, attach3) VALUES (:username, :project_id, :title, :description_data, :data_d, :attach1, :attach2, :attach3)');
