1cf5ed3851e1fbbac5ac62bfbbd9e2372cf8fbad
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-06-06 18:54:14 -0600

    syntax

--- web/Project01/DB_newProfile.php
+++ web/Project01/DB_newProfile.php
@@ -5,7 +5,7 @@
     $username = htmlspecialchars($_POST['username']);
     $password = password_hash(htmlspecialchars($_POST['password']), PASSWORD_DEFAULT);
 
-   $stmt = $db->prepare('INSERT INTO profiles user_name, password VALUES (:username, :password)');
+   $stmt = $db->prepare('INSERT INTO profiles (user_name, password) VALUES (:username, :password)');
    $stmt->bindValue(':username',$username,PDO::PARAM_STR);
    $stmt->bindValue(':password', $password, PDO::PARAM_STR);
    $stmt->execute();
